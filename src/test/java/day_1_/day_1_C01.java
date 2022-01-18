b21df3e66e6b321cc273f7c876fa91794bcc1d8d
mehmet surucu <mehmetsmarket@gmail.com>
2022-01-18 15:15:55 +0300

    first commit

---
+++ src/test/java/day_1_/day_1_C01.java
@@ -0,0 +1,19 @@
+package day_1_;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class day_1_C01 {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","resources/driver/chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        // biz driver objesi olusturdugumuzda bos bir chrome browser acilir
+
+        driver.get("https://www.amazon.com");
+        // driver.get method'u driver'a gidecegi web adresini girmemizi saglar
+
+        Thread.sleep(3000);
+
+        driver.close();
+        // driver'i kapatir
+    }
+}
